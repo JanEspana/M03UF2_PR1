4d67fc1e2cb3a90da26d29e837e3614b98a9ed9e
janespana <jan.espana.7e7@itb.cat>
2024-01-16 21:20:26 +0100

    Agregar archivos de proyecto.

---
+++ M03_UF2_PR1_ClassLibrary/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace M03_UF2_PR1_ClassLibrary
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ M03_UF2_PR1_TestProject/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace M03_UF2_PR1_TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
