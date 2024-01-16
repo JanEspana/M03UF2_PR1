d85bc02c5f9128784d575b3b1e827713ea954790
janespana <jan.espana.7e7@itb.cat>
2024-01-16 21:57:07 +0100

    Corregido inicio. Closes #3

--- M03UF2_PR1/Program.cs
+++ M03UF2_PR1/Program.cs
@@ -10,28 +10,40 @@ namespace EspañaJanUF2PR1
             const string INTRO = "Bienvenido a Héroes vs Monstruo. Qué quieres hacer?\n1. Empezar una nueva aventura\n0. Salir",
                 DIFF = "Elige un nivel de dificultad:\n1. Fácil\n2. Difícil\n3. Personalizado\n4. Aleatorio",
                 ERROR = "Valor incorrecto, vuelve a introducirlo.";
-            
-            int selection = 0, difficulty = 0, max, min;
+
+            int selection = 0, difficulty = 0, errorCount = 3, max, min;
             do
             {
-                Console.WriteLine(INTRO);
-                selection = Convert.ToInt32(Console.ReadLine());
-                if (!HeroeVSMonstruoLibrary.InRange(selection, max = 1, min = 0))
+                do
                 {
-                    Console.WriteLine(ERROR);
+                    Console.WriteLine(INTRO);
+                    selection = Convert.ToInt32(Console.ReadLine());
+                    if (!HeroeVSMonstruoLibrary.InRange(selection, max = 1, min = 0))
+                    {
+                        errorCount--;
+                        if (errorCount > 0)
+                        {
+                            Console.WriteLine(ERROR);
+                        }
+                    }
+                }
+                while (!HeroeVSMonstruoLibrary.InRange(selection, max = 1, min = 0) && errorCount > 0);
+
+                while (!HeroeVSMonstruoLibrary.InRange(difficulty, max = 4, min = 1) && errorCount > 0 && selection == 1)
+                {
+                    Console.WriteLine(DIFF);
+                    difficulty = Convert.ToInt32(Console.ReadLine());
+                    if (!HeroeVSMonstruoLibrary.InRange(difficulty, max = 4, min = 1))
+                    {
+                        errorCount--;
+                        if (errorCount > 0)
+                        {
+                            Console.WriteLine(ERROR);
+                        }
+                    }
                 }
             }
-            while (!HeroeVSMonstruoLibrary.InRange(selection, max = 1, min = 0));
-            do
-            {
-                Console.WriteLine(DIFF);
-                difficulty = Convert.ToInt32(Console.ReadLine());
-                if (!HeroeVSMonstruoLibrary.InRange(difficulty, max = 4, min = 1))
-                {
-                    Console.WriteLine(ERROR);
-                }
-            }
-            while (!HeroeVSMonstruoLibrary.InRange(difficulty, max = 4, min = 1));
+            while (errorCount < 0);
         }
     }
 }
