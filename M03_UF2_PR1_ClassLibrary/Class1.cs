5bd81f2e54d4ae6aa7af891945400857c9df390c
janespana <jan.espana.7e7@itb.cat>
2024-01-18 22:46:05 +0100

    Creados los turnos aleatorios y las stats aleatorias. Closes #12

--- M03_UF2_PR1_ClassLibrary/Class1.cs
+++ M03_UF2_PR1_ClassLibrary/Class1.cs
@@ -27,5 +27,22 @@ namespace M03_UF2_PR1_ClassLibrary
             Random rnd = new Random();
             return (double)rnd.Next(Convert.ToInt32(min), Convert.ToInt32(max) + 1);
         }
+        public static int[] Turns(int[] turns)
+        {
+            Random rnd = new Random();
+            int[] turnsRandom = new int[turns.Length];
+            for (int i = 0; i < turns.Length; i++)
+            {
+                turnsRandom[i] = rnd.Next(0, 4) + 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (turnsRandom[i] == turnsRandom[j])
+                    {
+                        i--;
+                    }
+                }
+            }
+            return turnsRandom;
+        }
     }
 }
