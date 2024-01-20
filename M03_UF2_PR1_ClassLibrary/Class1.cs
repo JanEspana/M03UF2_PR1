b44afdecbc132d04c714f23d83959af0921b6614
janespana <jan.espana.7e7@itb.cat>
2024-01-20 18:29:28 +0100

    Turnos actualizados y arreglados. Añadidos métodos de ataque y habilidad especial. Closes #15

--- M03_UF2_PR1_ClassLibrary/Class1.cs
+++ M03_UF2_PR1_ClassLibrary/Class1.cs
@@ -33,7 +33,7 @@ namespace M03_UF2_PR1_ClassLibrary
             int[] turnsRandom = new int[turns.Length];
             for (int i = 0; i < turns.Length; i++)
             {
-                turnsRandom[i] = rnd.Next(0, 4) + 1;
+                turnsRandom[i] = rnd.Next(0, 4);
                 for (int j = 0; j < i; j++)
                 {
                     if (turnsRandom[i] == turnsRandom[j])
@@ -43,6 +43,25 @@ namespace M03_UF2_PR1_ClassLibrary
                 }
             }
             return turnsRandom;
+        }
+        public static double HeroAttack(double atk, double monsterHp, double monsterDf)
+        {
+            return monsterHp - atk + (atk * monsterDf / 100) > 0 ? monsterHp - atk + (atk * monsterDf / 100) : 0;
+        }
+        public static double[] Heal(double[] hp, double[] maxHp)
+        {
+            for (int i = 0; i < hp.Length; i++)
+            {
+                if (hp[i] + 500 >= maxHp[i])
+                {
+                    hp[i] = maxHp[i];
+                }
+                else
+                {
+                    hp[i] = hp[i] + 500;
+                }
+            }
+            return hp;
         }
     }
 }
